f45737e6eee18ddeabb26b7fd44df53f55dd698e
John Demos <jdemos@twitter.com>
2017-07-07 16:16:52 -0600

    JD: Adds faraday on-behalf call in request header.

--- rules_stats.rb
+++ rules_stats.rb
@@ -6,32 +6,46 @@ require 'json'
 require 'faraday'
 require 'yaml'
 
-# Path to yaml file with credentials
-config = YAML.load_file('./config/p/private.yml')
+# Path to yaml file with credentials - alternatively, specify the file path as the first command line argument (ARGV[0])
+if ARGV[0].empty?
+	config = YAML.load_file('./config/account.yml')
+else
+	config = YAML.load_file(ARGV[0])
+end
 
 username = config['username']
 password = config['password']
 account_name = config['account_name']
 stream_label = config['stream_label']
 
-# Make the request
+# # Make the request
+# conn = Faraday.new(url: "https://gnip-api.twitter.com")
+# conn.basic_auth(username, password)
+# response = conn.get("/rules/powertrack/accounts/#{account_name}/publishers/twitter/#{stream_label}.json")
+
+
 conn = Faraday.new(url: "https://gnip-api.twitter.com")
 conn.basic_auth(username, password)
-response = conn.get("/rules/powertrack/accounts/#{account_name}/publishers/twitter/#{stream_label}.json")
+response = conn.get "/rules/powertrack/accounts/#{account_name}/publishers/twitter/#{stream_label}.json" do |req|
+  req.headers['X-On-Behalf-Of'] = 'jim'
+end
+
+# # Print the response - useful when debugging
+# puts response.body
 
 jresp = JSON.parse(response.body)
 
 sent = jresp['sent'].gsub(/\W+/, '')
 datestamp = sent[0..-7]
 
-fname = "#{stream_label}_rules_#{datestamp}.json"
+file_name = "#{stream_label}_rules_#{datestamp}.json"
 
-rules_file = File.open("./files/#{fname}", "w+")
+rules_file = File.open("./files/#{file_name}", "w+")
 rules_file.puts response.body
 rules_file.close
 
 # Read file, parse, extract and store rules in 'rules' array
-file = File.read("./files/#{fname}")
+file = File.read("./files/#{file_name}")
 json = JSON.parse(file)
 
 rules = json["rules"]
